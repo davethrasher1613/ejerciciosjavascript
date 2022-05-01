24081e48be3729f13e4da6aae6608bef61aa87d5
davethrasher1613 <daveangelofdeath@gmail.com>
2022-05-01 18:13:56 -0500

    tareas javascriptsexto trimestre2 descuento,primos

--- primos.js
+++ primos.js
@@ -1,3 +1,18 @@
-var x= ("siuuu")
+function numerosprimos(numeroingresado){
+let residuo=0
 
-console.log(x)
+for (let i = 1; i <= numeroingresado ; i++) {
+    if (numeroingresado % i === 0) {
+        residuo++
+    }
+
+}
+
+if (residuo==2) {
+    console.log(`El numero ${numeroingresado} si es un numero primo`)
+    
+}else{
+    console.log(`El numero ${numeroingresado} no es un numero primo`)
+}
+}
+console.log(numerosprimos(4))
