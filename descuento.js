f6ef60e12931451e7ad6280c41404d5f5265c59f
davethrasher1613 <daveangelofdeath@gmail.com>
2022-05-01 16:34:23 -0500

    tareas javascriptsexto trimestre2 descuento

---
+++ descuento.js
@@ -0,0 +1,9 @@
+function descuento(precio, descuento){
+var descuento=50
+const descuentoinicial=precio/descuento
+var preciofinal= 100/descuentoinicial
+console.log(`el precio ingresado es de ${precio}`)
+console.log(`el descuento es de ${descuento}`)
+return 'el precio con descuento  es de:' + preciofinal;
+}
+console.log(descuento(200,50))
