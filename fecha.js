c69f3d967d43958d4919cf32490c572c6228e314
davethrasher1613 <daveangelofdeath@gmail.com>
2022-05-02 10:29:01 -0500

    tareas javascriptsexto trimestre2 descuento,primos,fecha(pendiente)

--- fecha.js
+++ fecha.js
@@ -1,5 +1,51 @@
-function perro(nombre){
-return `mi perrita labradora se llama ${nombre}` 
+
+
+
+/* 
+function fecha(a){
+
+var fechaingresada= new Date(a)
+var fechadehoy=new Date("2022-05-02")
+console.log(fechaingresada);
+console.log(fechadehoy);
+console.log()
+
+var fechadelpc = fechadehoy.toISOString().split("T")[0]
+
+
+if (fechadehoy>fechaingresada) {
+    console.log("la fecha que coloco es anterior")
+}else if(fechadehoy<fechaingresada){
+    console.log("la fecha que coloco es posterior")
+}else{
+    console.log("la fecha que coloco es actual")
 }
-const nombre=perro("Luna")
-console.log(nombre)
+
+}
+fecha("2023-05-02") */
+
+
+
+
+
+/* function fecha(fecha){
+
+    var fechaingresada= new Date(fecha)
+    var fechadehoy=new Date()
+    console.log(fechaingresada);
+    console.log(fechadehoy);
+    console.log()
+    
+    var fechadelpc = fechadehoy.toISOString().split("T")[0]
+    
+    
+    if (fechadelpc<fechaingresada) {
+        console.log("la fecha que coloco es anterior")
+    }else if(fechadelpc>fechaingresada){
+        console.log("la fecha que coloco es posterior")
+    }else{
+        console.log("la fecha que coloco es actual")
+    }
+    
+    }
+    fecha("2023-05-02") */
